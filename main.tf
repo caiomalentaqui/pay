4ff7443d078c7c608b271242a1b9e440b59c6112
caiomalentaqui <caio.malentaqui@gmail.com>
2026-01-20 00:03:58 -0300

    ajustes

--- main.tf
+++ main.tf
@@ -8,3 +8,45 @@ resource "kubernetes_namespace_v1" "pay_platform" {
       name = "pay-infrastructure"
     }  
 }
+
+resource "kubernetes_namespace_v1" "messaging-ingestion" {
+    metadata {
+      name = "messaging-ingestion"
+    }  
+}
+
+resource "kubernetes_namespace_v1" "messaging-storage" {
+    metadata {
+      name = "messaging-storage"
+    }  
+}
+
+resource "kubernetes_deployment_v1" "nginx_test" {
+  metadata {
+    name      = "nginx-test"
+    namespace = kubernetes_namespace_v1.pay_platform.metadata[0].name
+  }
+
+  spec {
+    replicas = 1
+    selector {
+      match_labels = {
+        app = "nginx"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "nginx"
+        }
+      }
+      spec {
+        container {
+          image = "nginx:latest"
+          name  = "nginx"
+        }
+      }
+    }
+  }
+}
+
